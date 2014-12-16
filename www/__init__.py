0622165e10b8022e2706ed6e2818105d7fb68780
Richard Pope <richard@memespring.co.uk>
2014-12-16 16:38:18 +0000

    allow fonts to be loaded from elsewhere

--- www/__init__.py
+++ www/__init__.py
@@ -1,10 +1,12 @@
 import os
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
+from flask_restful.utils import cors
 
 #app
 app = Flask(__name__)
 app.config.from_object(os.environ.get('SETTINGS'))
 db = MongoEngine(app)
+cors = CORS(app, resources={r"/static/*": {"origins": "*"}})
 
 from www import views
