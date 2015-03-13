ed0ae218a6f0533d4324b2339ce1f9269670d978
Adam Shimali <adam.shimali@gmail.com>
2015-03-13 13:29:34 +0000

    add http basic auth

--- www/__init__.py
+++ www/__init__.py
@@ -29,3 +29,12 @@ from www import views
 
 from www.greenpaper.views import paper
 app.register_blueprint(paper)
+
+from flask.ext.basicauth import BasicAuth
+http_basic_user = os.environ.get('BASIC_AUTH_USERNAME')
+http_basic_password = os.environ.get('BASIC_AUTH_PASSWORD')
+if http_basic_user and http_basic_pasword:
+    app.config['BASIC_AUTH_USERNAME'] = http_basic_user
+    app.config['BASIC_AUTH_PASSWORD'] = http_basic_password
+    app.config['BASIC_AUTH_FORCE'] = True
+    basic_auth = BasicAuth(app)
